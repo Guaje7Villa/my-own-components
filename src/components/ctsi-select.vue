c098a0069811ae6a9f3606cf955455fa5e60fb21
zhaoxunan <zhaoxunan@ctsi.com>
2018-08-12 14:47:27 +0800

    init basic files

---
+++ src/components/ctsi-select.vue
@@ -0,0 +1,72 @@
+<template>
+    <Row style="margin-top:10px;">
+        <Col :span="spanVal" v-if="isMulti==false">
+            <Select v-model="model1" filterable placeholder="单选">
+                <Option v-for="item in dataList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+            </Select>
+        </Col>
+        <Col :span="spanVal" v-else>
+            <Select v-model="model2" filterable multiple placeholder="多选">
+                <Option v-for="item in dataList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+            </Select>
+        </Col>
+    </Row>
+</template>
+<script>
+    export default {
+        data () {
+            return {
+                dataList: [],
+                model1: '',
+                model2: []
+            }
+        },
+        props: ["isMulti","queryCond","spanVal"],
+        methods: {
+          queryData() {
+            //调用后台接口返回数据
+            console.log("查询参数如下：");
+            console.log("queryCond:"+this.queryCond);
+            let queryCond = this.queryCond;
+            // util.ajaxMethodWidthParams(url, "post", p).then(response => {
+            //   console.log("返回数据如下：");
+            //   console.log(response.data);
+            // });
+
+
+          
+
+            this.dataList = [
+                  {
+                      value: 'New York',
+                      label: 'New York'
+                  },
+                  {
+                      value: 'London',
+                      label: 'London'
+                  },
+                  {
+                      value: 'Sydney',
+                      label: 'Sydney'
+                  },
+                  {
+                      value: 'Ottawa',
+                      label: 'Ottawa'
+                  },
+                  {
+                      value: 'Paris',
+                      label: 'Paris'
+                  },
+                  {
+                      value: 'Canberra',
+                      label: 'Canberra'
+                  }
+            ]
+          
+          }
+        },
+        created() {
+          this.queryData();
+        }
+    }
+</script>
---
+++ src/views/selectDemoIndex.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="main"> 
+      <h1>自定义组件封装</h1>
+        <CtsiSelect 
+          :isMulti="isMulti"
+          :queryCond="queryCond"
+          :spanVal="spanVal"
+        ></CtsiSelect>
+    </div>
+</template>
+
+<script>
+import CtsiSelect from '../components/ctsi-select'
+
+export default {
+  data () {
+    return {
+      isMulti: true,
+      queryCond:'1',
+      spanVal:24
+    }
+  },
+  components: {
+    CtsiSelect
+  }
+}
+</script>
+
+<style scoped>
+  .main{
+    width: 80%;
+    height: 500px;
+    border: 1px solid #BBBBBB;
+    margin-left: 10%;
+    margin-top: 10%;
+  }
+
+  h1 {
+    text-align: center;
+  }
+</style>
+
+
